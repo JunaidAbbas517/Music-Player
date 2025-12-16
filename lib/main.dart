36c7e6ba0101776b673a94767e1ebe149130cddb
Junaid Abbas <junaidabbas.pk92@gmail.com>
2025-12-17 01:23:40 +0800

    Refactor app initialization
    Refactor main function to improve initialization and app structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:music_play/firebase_options.dart';
 import 'package:music_play/view/music_player_app.dart';
 import 'package:music_play/view_model/music_player_view_model.dart';
@@ -8,11 +9,16 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  SystemChrome.setPreferredOrientations([
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
-  ]).then((value) => runApp(const MyApp()));
+  ]);
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,16 +29,27 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<MusicPlayerAppViewModel>(
-          create: (context) => MusicPlayerAppViewModel(),
-        )
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+          create: (_) => MusicPlayerAppViewModel(),
         ),
-        home: const MusicPlayerScreen(),
+      ],
+      child: ScreenUtilInit(
+        // 📱 Base design size (phone)
+        designSize: const Size(375, 812), // iPhone X reference
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Music Player',
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.deepPurple,
+              ),
+            ),
+            home: const MusicPlayerScreen(),
+          );
+        },
       ),
     );
   }
